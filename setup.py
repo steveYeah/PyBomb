82dda77a141573a9dacf81d9141850a947dae407
Steve Hutchins <hutchinsteve@gmail.com>
2015-09-22 12:37:25 +0100

    v0.1.3

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
 config = {
     'name': 'pybomb',
     'description': 'Client for the Giant Bomb API',
-    'version': '0.1.2',
+    'version': '0.1.3',
     'long_description_markdown_filename': 'README.md',
     'license': "MIT",
     'author': 'Steve Hutchins',
     'author_email': 'hutchinsteve@gmail.com',
     'url': 'https://github.com/steveYeah/PyBomb',
-    'download_url': 'https://github.com/steveYeah/PyBomb/archive/v0.1.2.tar.gz',
+    'download_url': 'https://github.com/steveYeah/PyBomb/archive/v0.1.3.tar.gz',
     'keywords': ['giant', 'bomb', 'game', 'api', 'client'],
     'packages': ['pybomb', 'pybomb.clients'],
     'install_requires': [
