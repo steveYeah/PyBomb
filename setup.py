0ab3eb45e138dfe3e0bdca0987d3a0f92fe7b431
steveYeah <hutchinsteve@gmail.com>
2016-04-25 22:35:47 +0100

    Add user agent to request

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ def markdown_description(file_name):
     return desc
 
 
+version = '0.1.5'
+
+
 try:
     import pypandoc
     long_description = markdown_description('README.md')
@@ -19,13 +22,13 @@ except ImportError:
 config = {
     'name': 'pybomb',
     'description': 'Client for the Giant Bomb API',
-    'version': '0.1.5',
+    'version': '{0}'.format(version),
     'long_description': long_description,
     'license': "MIT",
     'author': 'Steve Hutchins',
     'author_email': 'hutchinsteve@gmail.com',
     'url': 'https://github.com/steveYeah/PyBomb',
-    'download_url': 'https://github.com/steveYeah/PyBomb/archive/v0.1.4.tar.gz',
+    'download_url': 'https://github.com/steveYeah/PyBomb/archive/v{0}.tar.gz'.format(version),
     'keywords': ['giant', 'bomb', 'game', 'api', 'client'],
     'packages': ['pybomb', 'pybomb.clients'],
     'install_requires': [
